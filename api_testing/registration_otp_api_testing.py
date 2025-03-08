0a77a811fedf7b75e7912faf3cb9520a5c0e45fc
amoghthusoo <amoghthusoo@gmail.com>
2025-03-08 22:28:45 +0530

    2228.08.03.2025

--- api_testing/registration_otp_api_testing.py
+++ api_testing/registration_otp_api_testing.py
@@ -1,9 +1,10 @@
 import requests
 
-url = "http://127.0.0.1:8000/api/generate_registration_otp/"
+# url = "http://127.0.0.1:8000/api/generate_registration_otp/"
+url = "https://student-tools-gules.vercel.app/api/generate_registration_otp/"
 
 data = {
-    "email" : "swdproject2025user1@gmail.com"
+    "email" : "2022btcse007@curaj.ac.in"
 }
 
 response = requests.post(url, data = data)
--- swd_backend/swd_backend/settings.py
+++ swd_backend/swd_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%ri^h$3ut-)c#gqrp3sl0c@$tp$4&k&1u9alle1nh8s4lh7%$7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "student-tools-sage.vercel.app"]  
 
 
 # Application definition
